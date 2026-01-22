6bb0dc14dcef7acda77a5e40067c8ddfd59c7026
Satvik Mishra <satvikmishra04@gmail.com>
2026-01-22 07:48:48 +0530

    Delete musiclibrary.py

--- musiclibrary.py
+++
@@ -1,7 +0,0 @@
-music={"echo": "https://www.youtube.com/watch?v=uGtBHWicztg",
-       "solas": "https://www.youtube.com/watch?v=ZT0aOrjIcVE",
-       "ankhiyon":"https://www.youtube.com/watch?v=KqpIIaCJggY",
-       "summertime":"https://www.youtube.com/watch?v=TdrL3QxjyVw",
-       "ruk":"https://www.youtube.com/watch?v=9XhkhCMHU7I",
-       "tmkoc":"https://www.youtube.com/watch?v=7b2BGM7r8pM"
-       }
